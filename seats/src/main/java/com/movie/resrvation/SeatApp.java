afa7c5eb65bc495fb186d036c4d942ce398fa7ff
levan <levantosina1992@gmail.com>
2024-09-25 18:38:13 +0300

    add Api-gateway

---
+++ seats/src/main/java/com/movie/resrvation/SeatApp.java
@@ -0,0 +1,13 @@
+package com.movie.resrvation;
+
+/**
+ * Hello world!
+ *
+ */
+public class SeatApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
