0b32eec37397142b9f43ba9d780e36932c12d45c
levan <levantosina1992@gmail.com>
2024-10-09 12:01:00 +0300

    add UsersDetailsService class

---
+++ users/src/main/java/com/movie/users/users/UsersDetailsService.java
@@ -0,0 +1,28 @@
+package com.movie.users.users;
+
+import com.movie.users.users.UserDAO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author DMITRII LEVKIN on 09/10/2024
+ * @project MovieReservationSystem
+ */
+@Service
+public class UsersDetailsService implements UserDetailsService {
+    private final UserDAO userDAO;
+
+    public UsersDetailsService(@Qualifier("userJdbc") UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userDAO.selectUserByEmail(username)
+                .orElseThrow(()->
+                        new UsernameNotFoundException("Username "+username+" not found"));
+    }
+}
