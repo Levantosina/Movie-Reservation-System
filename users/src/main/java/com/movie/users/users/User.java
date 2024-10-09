c819f46a7ed5aa317079065d4a514b8c409d4d45
levan <levantosina1992@gmail.com>
2024-10-09 11:56:14 +0300

    add user details , setup email as a unique

--- users/src/main/java/com/movie/users/users/User.java
+++ users/src/main/java/com/movie/users/users/User.java
@@ -3,30 +3,92 @@ package com.movie.users.users;
 import com.movie.users.roles.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 /**
  * @author DMITRII LEVKIN on 22/09/2024
  * @project MovieReservationSystem
  */
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "user_email_unique",
+        columnNames = "email")
+        })
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails  {
     @Id
     @SequenceGenerator(name="user_id_seq",
     sequenceName = "user_id_seq")
     @GeneratedValue(strategy = GenerationType.IDENTITY,
     generator = "user_id_seq")
+    @Column(
+            nullable = false
+    )
     private Long userId;
+    @Column(
+            nullable = false
+    )
     private String firstName;
+    @Column(
+            nullable = false
+    )
     private String lastName;
+    @Column(
+            nullable = false
+    )
     private String email;
+    @Column(
+            nullable = false
+    )
+    private String password;
 
     @ManyToOne
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
+
+
