5f9e60097c8d54f3f8c314c8c62c426efcfacf32
levan <levantosina1992@gmail.com>
2024-10-15 13:09:46 +0300

    add AdminRegistrationRequest

---
+++ users/src/main/java/com/movie/users/users/AdminRegistrationRequest.java
@@ -0,0 +1,31 @@
+package com.movie.users.users;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * @author DMITRII LEVKIN on 15/10/2024
+ * @project MovieReservationSystem
+ */
+public record AdminRegistrationRequest (
+        @NotBlank(message = "First name is required")
+        String firstName,
+        @NotBlank(message = "Last name is required")
+        String lastName,
+        @Email(message = "Please provide a valid email address")
+        @Pattern(
+                regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+                message = "Invalid email format"
+        )
+        @NotBlank(message = "Email is required")
+        String email,
+        @NotBlank(message = "Password is required")
+        String password,
+
+        @NotBlank(message = "Role name is required")
+        String roleName
+) {
+
+
+}
