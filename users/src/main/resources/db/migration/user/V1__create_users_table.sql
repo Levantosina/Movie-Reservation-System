5bf4b46ddc7c1a4c1982dde25b6830c5d9b3da79
levan <levantosina1992@gmail.com>
2024-10-15 13:09:25 +0300

    add custom value for role admin, setup password varchar(100)

--- users/src/main/resources/db/migration/user/V1__create_users_table.sql
+++ users/src/main/resources/db/migration/user/V1__create_users_table.sql
@@ -10,7 +10,7 @@ CREATE TABLE users(
                       first_name varchar(25) NOT NULL ,
                       last_name varchar(25)NOT NULL,
                       email varchar (50)NOT NULL UNIQUE ,
-                    password varchar NOT NULL,
+                    password varchar(100) NOT NULL,
                       role_id SERIAL NOT NULL ,
                       CONSTRAINT fk_role FOREIGN KEY (role_id)
                           REFERENCES roles (role_id)
@@ -20,3 +20,7 @@ CREATE TABLE users(
 
 INSERT INTO ROLES(role_id,role_name,description)VALUES (1,'ROLE_ADMIN','admin');
 INSERT INTO ROLES(role_id,role_name,description)VALUES (2,'ROLE_USER','user');
+
+INSERT INTO users (first_name, last_name, email, password, role_id)
+VALUES ('Admin', 'Admin', 'admin@admin.com', '$2a$10$YbWNcZ52v1Pr9AC/5ydq2eLd7bFt52BDDvEdiO8eKu/Gxq0PfNZ.K',
+        (SELECT roles.role_id FROM roles WHERE role_name = 'ROLE_ADMIN'));
