0c2a29c16d63c4efd382861122a8f9ca786454fb
levan <levantosina1992@gmail.com>
2024-10-15 13:08:06 +0300

    some fixes

--- users/src/main/java/com/movie/users/users/UserService.java
+++ users/src/main/java/com/movie/users/users/UserService.java
@@ -13,8 +13,6 @@ import com.movie.users.users.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
