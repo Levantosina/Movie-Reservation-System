d0479c69a0a7fe67ef054bbfe8c042f548cc410c
levan <levantosina1992@gmail.com>
2024-10-23 18:05:49 +0300

    add sql schedule

--- movieschedules/src/main/resources/db/migration/schedule/V1__ScheduleCreate.sql
+++ movieschedules/src/main/resources/db/migration/schedule/V1__ScheduleCreate.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS schedules;
+
 CREATE TABLE schedules (
                            schedule_id SERIAL PRIMARY KEY,
                            movie_id INT NOT NULL,
@@ -7,4 +7,8 @@ CREATE TABLE schedules (
                            start_time TIME NOT NULL,
                            end_time TIME NOT NULL,
                            available_seats INT NOT NULL
+
 );
+
+INSERT INTO schedules(movie_id, cinema_id, date, start_time, end_time, available_seats) VALUES (1,1,'12.03.2024','10:00:00','12:00:00',22);
+
