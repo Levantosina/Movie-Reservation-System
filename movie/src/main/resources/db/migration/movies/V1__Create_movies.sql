2127cf96e5c5da7b654b79f2ace7d51fe6a75139
levan <levantosina1992@gmail.com>
2024-10-24 16:02:57 +0300

    setup movie name like a unique

--- movie/src/main/resources/db/migration/movies/V1__Create_movies.sql
+++ movie/src/main/resources/db/migration/movies/V1__Create_movies.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
                         movie_id SERIAL PRIMARY KEY,
-                        movie_name TEXT NOT NULL,
+                        movie_name TEXT NOT NULL UNIQUE,
                         year INTEGER NOT NULL,
                         country TEXT NOT NULL,
                         genre TEXT NOT NULL,
