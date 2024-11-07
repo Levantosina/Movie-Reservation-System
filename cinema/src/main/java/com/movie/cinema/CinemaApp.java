fb879b43dc94dd5edc6381bbfc08da7d32275993
levan <levantosina1992@gmail.com>
2024-11-07 12:17:37 +0200

    add @EnableFeignClients

--- cinema/src/main/java/com/movie/cinema/CinemaApp.java
+++ cinema/src/main/java/com/movie/cinema/CinemaApp.java
@@ -5,15 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication(scanBasePackages = {
 
         "com.movie.cinema",
         "com.movie.amqp",
-        "com.movie.users"
+        "com.movie.users",
+        "com.movie.jwt.jwt",
+        "com.movie.client"
 })
 @EnableDiscoveryClient
+@EnableFeignClients(
+        basePackages = "com.movie.client"
+)
 @EntityScan(basePackages = "com.movie.cinema.cinema.entity")
 public class CinemaApp
 {
