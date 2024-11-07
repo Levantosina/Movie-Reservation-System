4a00fb869ff4f889fcc8b683839ec2d95db6e4ae
levan <levantosina1992@gmail.com>
2024-11-07 16:34:08 +0200

    add MovieUsersDetailsService

--- seat/src/main/java/com/movie/seats/security/SeatSecurityConfig.java
+++ seat/src/main/java/com/movie/seats/security/SeatSecurityConfig.java
@@ -2,6 +2,8 @@ package com.movie.seats.security;
 
 
 
+
+import com.movie.client.MovieUsersDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +20,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SeatSecurityConfig {
 
+    private final MovieUsersDetailsService userDetailsService;
+
+    public SeatSecurityConfig(MovieUsersDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -31,15 +39,10 @@ public class SeatSecurityConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(
-           UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder
-    ) {
-        DaoAuthenticationProvider daoAuthenticationProvider =
-                new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
-
 }
