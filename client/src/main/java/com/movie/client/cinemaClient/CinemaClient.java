dedf75db4d748b2bb31d361d923055e286d01033
levan <levantosina1992@gmail.com>
2024-11-07 16:34:49 +0200

    create CinemaClient

--- client/src/main/java/com/movie/client/cinemaClient/CinemaClient.java
+++ client/src/main/java/com/movie/client/cinemaClient/CinemaClient.java
@@ -2,23 +2,21 @@ package com.movie.client.cinemaClient;
 
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 /**
  * @author DMITRII LEVKIN on 04/10/2024
  * @project MovieReservationSystem
  */
-@FeignClient(name = "cinemas",url = "http://localhost:8081")
-@Component
-public interface CinemaClient {
-    @GetMapping("/api/v1/cinemas/name/{cinemaName}")
-    void getCinemaByName(@PathVariable("cinemaName") String cinemaName);
+@FeignClient(value = "cinema")
 
-    @GetMapping("/api/v1/cinemas/id-by-name/{cinemaName}")
+public interface CinemaClient {
+    @GetMapping(value= "/api/v1/cinemas/id-by-name/{cinemaName}")
     Long getCinemaIdByName(@PathVariable("cinemaName") String cinemaName);
 }
 
