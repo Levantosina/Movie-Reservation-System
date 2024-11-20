774e5a75d1a10ffc60586c73ad86eede456e8e4a
levan <levantosina1992@gmail.com>
2024-11-20 15:25:34 +0200

    fixes

--- user/src/test/java/com/movie/users/it/AdminServiceIntegrationTest.java
+++ user/src/test/java/com/movie/users/it/AdminServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.movie.users.it;
 
 import com.github.javafaker.Faker;
+import com.movie.amqp.RabbitMqMessageProducer;
+import com.movie.client.notification.NotificationRequest;
 import com.movie.common.AuthenticationRequest;
 import com.movie.common.UserDTO;
 import com.movie.users.AbstractDaoUnitTest;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -18,6 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 /**
@@ -36,37 +40,39 @@ public class AdminServiceIntegrationTest extends AbstractDaoUnitTest {
     private static String ADMIN_PATH = "/api/v1/admin";
     private static String jwtToken;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void canRegisterNewAdminAndPublishNotification() {
 
-        String email = "admin@gmail.com";
+        Faker faker = new Faker();
+        String firstName = "Test";
+        String lastName = "Test";
+        String email = faker.name().lastName() + "-" + UUID.randomUUID() + "@gmail.com";
+        String role = "ROLE_ADMIN";
+        String password = "password";
+
         AdminRegistrationRequest adminRegistrationRequest = new AdminRegistrationRequest(
-                "Admin", "Test", email, "password", "ROLE_ADMIN"
+                firstName, lastName, email, password, role
         );
 
         User currentAdmin = new User();
         currentAdmin.setEmail(email);
         currentAdmin.setRole(Role.ROLE_ADMIN);
+
+
+        RabbitMqMessageProducer rabbitMqMessageProducerMock = mock(RabbitMqMessageProducer.class);
+        ReflectionTestUtils.setField(adminService, "rabbitMqMessageProducer", rabbitMqMessageProducerMock);
+
+
         adminService.registerAdministrator(adminRegistrationRequest, currentAdmin);
 
-        String userEmail = "user1@gmail.com";
-        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest(
-                "USER", "USER", userEmail, "password", "ROLE_USER");
-        User currentUser= new User();
-        currentUser.setEmail(userEmail);
-        currentUser.setRole(Role.ROLE_USER);
-        userService.registerUser(userRegistrationRequest);
-    }
+        verify(rabbitMqMessageProducerMock, times(1)).publish(
+                any(NotificationRequest.class),
+                eq("internal.exchange"),
+                eq("internal.notification.routing-key")
+        );
 
-    @Test
-    void canRegisterNewAdmin() {
-        Faker faker =new Faker();
-        String adminUsername = "admin@gmail.com";
-        String adminPassword = "password";
-
-        AuthenticationRequest loginRequest = new AuthenticationRequest(adminUsername, adminPassword);
-
-        jwtToken = webTestClient.post()
+        AuthenticationRequest loginRequest = new AuthenticationRequest(email, password);
+        String jwtToken = webTestClient.post()
                 .uri("/api/v1/auth/login")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -79,19 +85,23 @@ public class AdminServiceIntegrationTest extends AbstractDaoUnitTest {
                 .get(AUTHORIZATION)
                 .get(0);
 
-        AdminRegistrationRequest adminRegistrationRequest = new AdminRegistrationRequest(
-                "Admin", "Admin", "newbla@gmail.com", "password", "ROLE_ADMIN"
+
+        String email1 = faker.name().lastName() + "-" + UUID.randomUUID() + "@gmail.com";
+        AdminRegistrationRequest adminRegistrationRequest1 = new AdminRegistrationRequest(
+                "Admin", "Admin", email1, "password", "ROLE_ADMIN"
         );
 
+        // Register the new admin
         webTestClient.post()
                 .uri(ADMIN_PATH)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, "Bearer " + jwtToken)
-                .body(Mono.just(adminRegistrationRequest), AdminRegistrationRequest.class)
+                .body(Mono.just(adminRegistrationRequest1), AdminRegistrationRequest.class)
                 .exchange()
                 .expectStatus()
                 .isOk();
+
 
         List<UserDTO> allUsers = webTestClient.get()
                 .uri(ADMIN_PATH)
@@ -104,16 +114,20 @@ public class AdminServiceIntegrationTest extends AbstractDaoUnitTest {
                 .returnResult()
                 .getResponseBody();
 
+
         long adminId = allUsers.stream()
-                .filter(admin -> admin.email().equals(adminRegistrationRequest.email()))
+                .filter(admin -> admin.email().equals(adminRegistrationRequest1.email()))
                 .map(UserDTO::userId)
                 .findFirst()
                 .orElseThrow(() -> new AssertionError("Admin not found"));
 
-        UserDTO expectedAdmin = new UserDTO(adminId, adminRegistrationRequest.firstName(),
-                adminRegistrationRequest.lastName(), adminRegistrationRequest.email(),
-                adminRegistrationRequest.roleName());
+        UserDTO expectedAdmin = new UserDTO(adminId, adminRegistrationRequest1.firstName(),
+                adminRegistrationRequest1.lastName(), adminRegistrationRequest1.email(),
+                adminRegistrationRequest1.roleName());
+
+
         assertThat(allUsers).contains(expectedAdmin);
+
 
         webTestClient.get()
                 .uri(ADMIN_PATH + "/{id}", adminId)
@@ -127,11 +141,31 @@ public class AdminServiceIntegrationTest extends AbstractDaoUnitTest {
     }
     @Test
     void canUpdateUserRole() {
-        String adminUsername = "admin@gmail.com";
-        String adminPassword = "password";
+
+        String email = "admin@gmail.com";
+        String password = "password";
+        AdminRegistrationRequest adminRegistrationRequest = new AdminRegistrationRequest(
+                "Admin", "Test", email, "password", "ROLE_ADMIN"
+        );
+
+        User currentAdmin = new User();
+        currentAdmin.setEmail(email);
+        currentAdmin.setRole(Role.ROLE_ADMIN);
+        adminService.registerAdministrator(adminRegistrationRequest, currentAdmin);
+
+        String userEmail = "user1@gmail.com";
+        String userPassword = "password";
+
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest(
+                "USER", "USER", userEmail, userPassword, "ROLE_USER");
+        User currentUser= new User();
+        currentUser.setEmail(userEmail);
+        currentUser.setRole(Role.ROLE_USER);
+        userService.registerUser(userRegistrationRequest);
 
 
-        AuthenticationRequest loginRequest = new AuthenticationRequest(adminUsername, adminPassword);
+
+        AuthenticationRequest loginRequest = new AuthenticationRequest(email, password);
 
         String jwtToken = webTestClient.post()
                 .uri("/api/v1/auth/login")
@@ -148,10 +182,9 @@ public class AdminServiceIntegrationTest extends AbstractDaoUnitTest {
 
 
         RoleRequestDTO roleRequest = new RoleRequestDTO("ROLE_ADMIN");
-        String userEmailToUpdate = "user1@gmail.com";
 
         webTestClient.post()
-                .uri("/api/v1/admin/users/{email}/roles", userEmailToUpdate)
+                .uri("/api/v1/admin/users/{email}/roles", userEmail)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, "Bearer " + jwtToken)
