ec739bda72731a852ae7d2c094db68717b2e46b8
levan <levantosina1992@gmail.com>
2024-11-27 16:34:07 +0200

    change the generator type to IDENTITY

--- movie/src/main/java/com/movie/movie/movie/Movie.java
+++ movie/src/main/java/com/movie/movie/movie/Movie.java
@@ -3,6 +3,8 @@ package com.movie.movie.movie;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 /**
  * @author DMITRII LEVKIN on 30/09/2024
  * @project MovieReservationSystem
@@ -21,16 +23,49 @@ public class Movie {
     @Id
     @SequenceGenerator(
             name = "movies_id_seq",
-            sequenceName = "movies_id_seq"
+            sequenceName = "movies_id_seq",
+            allocationSize = 1
     )
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    @GeneratedValue(strategy = GenerationType.IDENTITY,
                     generator = "movies_id_seq")
+    @Column(
+            nullable = false
+    )
     private Long movieId;
+    @Column(
+            nullable = false
+    )
     private String movieName;
+    @Column(
+            nullable = false
+    )
     private Integer year;
+    @Column(
+            nullable = false
+    )
     private String country;
+    @Column(
+            nullable = false
+    )
     private String genre;
+    @Column(
+            nullable = false
+    )
     private String description;
 
 
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(movieId, movie.movieId) && Objects.equals(movieName, movie.movieName) && Objects.equals(year, movie.year) && Objects.equals(country, movie.country) && Objects.equals(genre, movie.genre) && Objects.equals(description, movie.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieId, movieName, year, country, genre, description);
+    }
 }
