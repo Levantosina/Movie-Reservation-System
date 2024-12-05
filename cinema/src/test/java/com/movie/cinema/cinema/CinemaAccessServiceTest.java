c66bc2fa291211ea5f770537aeb79ac7d0e1164f
levan <levantosina1992@gmail.com>
2024-12-05 13:09:18 +0200

    create CinemaAccessServiceTest

---
+++ cinema/src/test/java/com/movie/cinema/cinema/CinemaAccessServiceTest.java
@@ -0,0 +1,71 @@
+package com.movie.cinema.cinema;
+
+import com.movie.cinema.CinemaAbstractDaoUnitTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author DMITRII LEVKIN on 04/12/2024
+ * @project Movie-Reservation-System
+ */
+class CinemaAccessServiceTest extends CinemaAbstractDaoUnitTest {
+
+    private CinemaAccessService underTest;
+    private final CinemaRowMapper cinemaRowMapper = new CinemaRowMapper();
+
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CinemaAccessService(getJdbcTemplate(),
+                cinemaRowMapper);
+    }
+
+
+
+//        Cinema cinema = Cinema.builder()
+//
+//                .cinemaLocation("NY")
+//                .cinemaName("Amazonia")
+//                .build();
+//        underTest.insertCinema(cinema);
+//        List<Cinema> actual = underTest.selectAllCinemas();
+//        assertThat(actual).isNotEmpty();
+
+
+    @Test
+    void selectAllCinemas() {
+
+        Cinema cinema = Cinema.builder()
+                .cinemaName("")
+                .cinemaLocation("")
+                .build();
+
+        underTest.insertCinema(cinema);
+        List<Cinema> actual = underTest.selectAllCinemas();
+        assertThat(actual).isNotEmpty();
+    }
+
+    @Test
+    void selectCinemaById() {
+    }
+
+    @Test
+    void selectCinemaByCinemaName() {
+    }
+
+    @Test
+    void insertCinema() {
+    }
+
+    @Test
+    void existCinemaWithName() {
+    }
+
+    @Test
+    void updateCinema() {
+    }
+}
