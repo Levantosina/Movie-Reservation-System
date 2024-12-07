c1225eb294787fab9044d6795a5b1ec872333536
levan <levantosina1992@gmail.com>
2024-12-07 10:59:36 +0200

    add validation

--- movie/src/main/java/com/movie/movie/movie/MovieRegistrationRequest.java
+++ movie/src/main/java/com/movie/movie/movie/MovieRegistrationRequest.java
@@ -1,14 +1,25 @@
 package com.movie.movie.movie;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 /**
  * @author DMITRII LEVKIN on 30/09/2024
  * @project MovieReservationSystem
  */
-public record MovieRegistrationRequest (
+public record MovieRegistrationRequest(
+        @NotBlank(message = "Movie name is required")
         String movieName,
+
+        @NotNull(message = "Year is required")
         Integer year,
+
+        @NotBlank(message = "Country is required")
         String country,
+
+        @NotBlank(message = "Genre is required")
         String genre,
+
+        @NotBlank(message = "Description is required")
         String description
-){
-}
+) {}
