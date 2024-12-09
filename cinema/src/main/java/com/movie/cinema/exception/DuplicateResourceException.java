6bc7a2b66e3d61e355451be088eafcf68fa8c61c
levan <levantosina1992@gmail.com>
2024-12-09 17:54:15 +0200

    create DuplicateResourceException

---
+++ cinema/src/main/java/com/movie/cinema/exception/DuplicateResourceException.java
@@ -0,0 +1,15 @@
+package com.movie.cinema.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author DMITRII LEVKIN on 09/12/2024
+ * @project Movie-Reservation-System
+ */
+@ResponseStatus(code= HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException{
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
