e3656f42d6fe7bdfd8fbb05141e922f675b13867
levan <levantosina1992@gmail.com>
2024-12-09 17:56:59 +0200

    add validation

--- cinema/src/main/java/com/movie/cinema/cinema/CinemaRegistrationRequest.java
+++ cinema/src/main/java/com/movie/cinema/cinema/CinemaRegistrationRequest.java
@@ -1,11 +1,15 @@
 package com.movie.cinema.cinema;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * @author DMITRII LEVKIN on 24/09/2024
  * @project MovieReservationSystem
  */
 public record CinemaRegistrationRequest(
+        @NotBlank(message = "Cinema name is required")
         String cinemaName,
+        @NotBlank(message = "Cinema Location is required")
         String cinemaLocation) {
 
 
