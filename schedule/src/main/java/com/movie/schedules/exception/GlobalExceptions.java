7f27071b275cc80a53d31eff3579e822eb84246c
levan <levantosina1992@gmail.com>
2024-12-19 16:29:17 +0200

    create GlobalExceptions

---
+++ schedule/src/main/java/com/movie/schedules/exception/GlobalExceptions.java
@@ -0,0 +1,19 @@
+package com.movie.schedules.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author DMITRII LEVKIN on 19/12/2024
+ * @project Movie-Reservation-System
+ */
+@ControllerAdvice
+public class GlobalExceptions {
+    @ExceptionHandler(ScheduleRequestValidationException.class)
+    public ResponseEntity<String> handleScheduleRequestValidationException(ScheduleRequestValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
+
