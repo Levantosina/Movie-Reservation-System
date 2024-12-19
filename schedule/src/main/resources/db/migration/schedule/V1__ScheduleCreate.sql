dd925e9701d5c22a2649b0f45faac3b0a45b4e52
levan <levantosina1992@gmail.com>
2024-12-19 17:43:16 +0200

    fix

--- schedule/src/main/resources/db/migration/schedule/V1__ScheduleCreate.sql
+++ schedule/src/main/resources/db/migration/schedule/V1__ScheduleCreate.sql
@@ -1,13 +1,12 @@
 
 CREATE TABLE schedules (
                            schedule_id SERIAL PRIMARY KEY,
-                           movie_id INT NOT NULL,
-                           cinema_id INT NOT NULL,
                            date DATE NOT NULL,
                            start_time TIME NOT NULL,
                            end_time TIME NOT NULL,
-                           available_seats INT NOT NULL
-
+                           available_seats INT NOT NULL,
+                            cinema_id INT NOT NULL,
+                            movie_id INT NOT NULL
 );
 
 INSERT INTO schedules(movie_id, cinema_id, date, start_time, end_time, available_seats) VALUES (1,1,'12.03.2024','10:00:00','12:00:00',22);
