86e115fe5156ecd2f5add54dc65c355c65cbe878
levan <levantosina1992@gmail.com>
2024-12-24 14:09:03 +0200

    fix

--- schedule/src/main/java/com/movie/schedules/movieschedules/MovieMovieScheduleAccessService.java
+++ schedule/src/main/java/com/movie/schedules/movieschedules/MovieMovieScheduleAccessService.java
@@ -1,6 +1,8 @@
 package com.movie.schedules.movieschedules;
 
 
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +14,7 @@ import java.util.Optional;
  * @author DMITRII LEVKIN on 01/10/2024
  * @project MovieReservationSystem
  */
+@Slf4j
 @Repository("scheduleJdbc")
 public class MovieMovieScheduleAccessService implements MovieScheduleDAO {
 
@@ -33,16 +36,19 @@ public class MovieMovieScheduleAccessService implements MovieScheduleDAO {
     @Override
     public void createSchedule(MovieSchedule movieSchedule) {
        String sql = """
-            INSERT INTO schedules (movie_id, cinema_id, date, start_time, end_time,available_seats)
+            INSERT INTO schedules (date, start_time, end_time,available_seats,cinema_id,movie_id)
             VALUES (?, ?, ?, ?, ?, ?)
             """;
         jdbcTemplate.update(sql,
-                movieSchedule.getMovieId(),
-                movieSchedule.getCinemaId(),
+
                 movieSchedule.getDate(),
                 movieSchedule.getStartTime(),
                 movieSchedule.getEndTime(),
-                movieSchedule.getAvailableSeats());
+                movieSchedule.getAvailableSeats(),
+                movieSchedule.getCinemaId(),
+                movieSchedule.getMovieId());
+
+        log.info("Schedule created: " + movieSchedule);
     }
     @Override
     public List<MovieSchedule> selectAllSchedules() {
@@ -52,18 +58,65 @@ public class MovieMovieScheduleAccessService implements MovieScheduleDAO {
         return jdbcTemplate.query(sql,movieScheduleRowMapper);
     }
 
-    @Override
-    public void upDateSchedule(MovieSchedule movieSchedule) {
-        String sql = "UPDATE schedules SET movie_id = ?, cinema_id = ?, date = ?, start_time = ?, end_time = ? WHERE schedule_id = ?";
-        jdbcTemplate.update(sql,
-                movieSchedule.getMovieId(),
-                movieSchedule.getCinemaId(),
-                movieSchedule.getDate(),
-                movieSchedule.getStartTime(),
-                movieSchedule.getEndTime(),
-                movieSchedule.getScheduleId());
-    }
+    @Transactional
+    public void upDateSchedule(MovieSchedule updatedSchedule) {
 
+        if (updatedSchedule.getDate()!= null) {
+            var sql = """
+            UPDATE schedules SET  date =? WHERE schedule_id =?
+            """;
+            jdbcTemplate.update(
+                    sql,
+                    updatedSchedule.getDate(),
+                    updatedSchedule.getScheduleId());
+        }
+
+        if (updatedSchedule.getStartTime() != null) {
+            var sql = """
+                    UPDATE schedules SET  start_time = ? WHERE schedule_id = ?;
+                    """;
+            jdbcTemplate.update(sql,
+                    updatedSchedule.getStartTime(),
+                    updatedSchedule.getScheduleId());
+        }
+
+        if (updatedSchedule.getEndTime() != null) {
+            var sql = """
+                    UPDATE schedules SET  end_time = ? WHERE schedule_id = ?
+                    """;
+
+            jdbcTemplate.update(sql,
+                    updatedSchedule.getEndTime(),
+                    updatedSchedule.getScheduleId());
+        }
+
+        if (updatedSchedule.getAvailableSeats() != null) {
+            var sql = """
+            UPDATE schedules SET  available_seats = ? WHERE schedule_id = ?
+            """;
+            jdbcTemplate.update(sql,
+                    updatedSchedule.getAvailableSeats(),
+                    updatedSchedule.getScheduleId());
+        }
+
+        if (updatedSchedule.getCinemaId() != null) {
+            var sql = """
+                    UPDATE schedules SET  cinema_id = ? WHERE schedule_id = ?
+                    """;
+            jdbcTemplate.update(sql,
+                    updatedSchedule.getCinemaId(),
+                    updatedSchedule.getScheduleId());
+        }
+
+        if (updatedSchedule.getMovieId() != null) {
+            var sql = """
+            UPDATE schedules SET  movie_id = ? WHERE schedule_id = ?
+            """;
+            jdbcTemplate.update(sql,
+                    updatedSchedule.getMovieId(),
+                    updatedSchedule.getScheduleId());
+        }
+    }
     @Override
     public Optional<MovieSchedule> selectScheduleById(Long scheduleId) {
         String sql = "SELECT * FROM schedules WHERE schedule_id = ?";
