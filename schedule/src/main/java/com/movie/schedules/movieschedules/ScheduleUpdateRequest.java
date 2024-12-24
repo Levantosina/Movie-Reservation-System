3cc81b914eb82ae66d604cae2f19102a0e0e2945
levan <levantosina1992@gmail.com>
2024-12-24 14:16:46 +0200

    add validation

--- schedule/src/main/java/com/movie/schedules/movieschedules/ScheduleUpdateRequest.java
+++ schedule/src/main/java/com/movie/schedules/movieschedules/ScheduleUpdateRequest.java
@@ -1,5 +1,9 @@
 package com.movie.schedules.movieschedules;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -8,10 +12,27 @@ import java.time.LocalTime;
  * @project Movie-Reservation-System
  */
 public record ScheduleUpdateRequest (
-    LocalDate date,
-    LocalTime startTime,
-    LocalTime endTime,
-    Integer availableSeats,
-    Long cinemaId,
-    Long movieId
+        @NotNull(message = "Date cannot be null")
+        LocalDate date,
+
+        @JsonProperty("start_time")
+        @NotNull(message = "Start time cannot be null")
+        LocalTime startTime,
+
+        @JsonProperty("end_time")
+        @NotNull(message = "End time cannot be null")
+        LocalTime endTime,
+
+        @JsonProperty("available_seats")
+        @NotNull(message = "Available seats cannot be null")
+        @Positive(message = "Available seats must be a positive number")
+        Integer availableSeats,
+
+        @JsonProperty("cinema_id")
+        @Positive(message = "Cinema ID must be a positive number")
+        Long cinemaId,
+
+        @JsonProperty("movie_id")
+        @Positive(message = "Movie ID must be a positive number")
+        Long movieId
 ){ }
