e3fd42a8dc8be212cbe01bcc064a13fe768f44cd
levan <levantosina1992@gmail.com>
2024-12-29 15:21:18 +0200

    create seatType class

---
+++ seat/src/main/java/com/movie/seats/seat/SeatType.java
@@ -0,0 +1,35 @@
+package com.movie.seats.seat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * @author DMITRII LEVKIN on 29/12/2024
+ * @project Movie-Reservation-System
+ */
+@Getter
+public enum SeatType {
+
+    STANDARD("Standard", BigDecimal.valueOf(15.00)),
+    VIP("VIP", BigDecimal.valueOf(22.00)),
+    DISABLED("Disabled", BigDecimal.valueOf(10.00));
+
+    private final String type;
+    private final BigDecimal price;
+
+    SeatType(String type, BigDecimal price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public static BigDecimal getPriceByType(String type) {
+        for (SeatType seatType : values()) {
+            if (seatType.getType().equalsIgnoreCase(type)) {
+                return seatType.getPrice();
+            }
+        }
+        throw new IllegalArgumentException("Invalid seat type: " + type);
+    }
+}
