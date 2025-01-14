54e45aec744de04414f51a5dc1a1a4d897aa2e27
levan <levantosina1992@gmail.com>
2025-01-14 17:21:21 +0200

    add new validation

--- ticket/src/main/java/com/movie/ticket/ticket/TicketService.java
+++ ticket/src/main/java/com/movie/ticket/ticket/TicketService.java
@@ -14,12 +14,18 @@ import com.movie.common.UserDTO;
 import com.movie.exceptions.AlreadyOccupiedException;
 import com.movie.exceptions.DuplicateResourceException;
 import com.movie.exceptions.ResourceNotFoundException;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.NodeSetData;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,9 +52,7 @@ public class TicketService {
     private final ScheduleClient scheduleClient;
 
 
-
-
-    public TicketService(@Qualifier("ticketJdbc")TicketDAO ticketDAO, TicketDTOMapper ticketDTOMapper, RabbitMqMessageProducer rabbitMqMessageProducer, UserClient userClient, MovieClient movieClient, SeatClient seatClient, ScheduleClient scheduleClient) {
+    public TicketService(@Qualifier("ticketJdbc") TicketDAO ticketDAO, TicketDTOMapper ticketDTOMapper, RabbitMqMessageProducer rabbitMqMessageProducer, UserClient userClient, MovieClient movieClient, SeatClient seatClient, ScheduleClient scheduleClient) {
         this.ticketDAO = ticketDAO;
 
         this.ticketDTOMapper = ticketDTOMapper;
@@ -62,68 +66,84 @@ public class TicketService {
 
     public List<TicketDTO> getAllTickets() {
 
-        return  ticketDAO.selectAllTickets().stream()
+        return ticketDAO.selectAllTickets().stream()
                 .map(ticketDTOMapper)
                 .collect(Collectors.toList());
     }
 
-    public void createTicket(TicketRegistrationRequest ticketRegistrationRequest){
+    public void createTicket(TicketRegistrationRequest ticketRegistrationRequest) {
 
         Ticket ticket = new Ticket();
 
+        // Get the current auth_user
         String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+
+
+        // Retrieve user
         UserDTO userDTO = userClient.getUserByUsername(username);
         if (userDTO == null) {
             throw new ResourceNotFoundException("User not found.");
         }
 
 
+        //fetch seat
         SeatDTO seatDTO = seatClient.getSeatById(ticketRegistrationRequest.seatId());
         if (seatDTO == null) {
             throw new ResourceNotFoundException("Seat not found.");
         }
+
+        // retrieve price
+        BigDecimal price = seatClient.getSeatPriceById(ticketRegistrationRequest.seatId());
+        if (price == null) {
+            throw new ResourceNotFoundException("Price not found for the selected seat.");
+        }
+
 
         ScheduleDTO scheduleDTO = scheduleClient.getScheduleById(ticketRegistrationRequest.scheduleId());
         if (scheduleDTO == null) {
             throw new ResourceNotFoundException("Schedule not found.");
         }
 
+        // validation
         MovieDTO movieDTO = movieClient.getMovieById(ticketRegistrationRequest.movieId());
         if (movieDTO == null) {
             throw new ResourceNotFoundException("Movie not found.");
         }
 
-        Long userId = userDTO.userId();
-
+        //validation
         if (scheduleDTO.availableSeats() <= 0) {
             throw new AlreadyOccupiedException("Tickets are sold out for this schedule.");
         }
 
-        if(seatDTO.isOccupied())
+        // validation
+        if (seatDTO.isOccupied()) {
             throw new AlreadyOccupiedException("The selected seat is already occupied.");
+        }
 
+        //validation
         if (!scheduleDTO.cinemaId().equals(ticketRegistrationRequest.cinemaId())) {
             throw new AlreadyOccupiedException("The schedule does not belong to the selected cinema.");
         }
 
-
+        // decrease available seats
         scheduleClient.decreaseAvailableSeats(ticketRegistrationRequest.scheduleId());
+
+        // Mark the seat
         seatClient.updateSeatOccupation(ticketRegistrationRequest.seatId(), true);
 
 
-
-        ticket.setUserId(userId);
+        ticket.setUserId(userDTO.userId());
         ticket.setMovieId(ticketRegistrationRequest.movieId());
         ticket.setCinemaId(ticketRegistrationRequest.cinemaId());
         ticket.setSeatId(ticketRegistrationRequest.seatId());
         ticket.setScheduleId(ticketRegistrationRequest.scheduleId());
-        ticket.setPrice(ticketRegistrationRequest.price());
-        ticket.setDate(ticketRegistrationRequest.date());
-        log.info("TICKET:{}", ticket);
+        ticket.setPrice(price);
+        ticket.setDate(new Date());
+
+        log.info("NEW TICKET: {}", ticket);
+
         ticketDAO.createOneTicket(ticket);
-
-
         NotificationRequest notificationRequest = new NotificationRequest(
                 ticket.getTicketId(), "New Ticket created", "The ticket has been created."
         );
@@ -133,7 +153,5 @@ public class TicketService {
                 "internal.exchange",
                 "internal.notification.routing-key"
         );
-
     }
-
 }
