a14633d11ee6890dd271e60045ece14e9c63eaec
levan <levantosina1992@gmail.com>
2025-01-15 19:14:16 +0200

    add getMovieNameById

--- client/src/main/java/com/movie/client/movieClient/MovieClient.java
+++ client/src/main/java/com/movie/client/movieClient/MovieClient.java
@@ -6,17 +6,21 @@ import com.movie.jwt.jwt.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author DMITRII LEVKIN on 24/12/2024
  * @project Movie-Reservation-System
  */
-@FeignClient(name = "movie",configuration = FeignConfig.class)
+@FeignClient(name = "movie", configuration = FeignConfig.class)
 public interface MovieClient {
 
     @GetMapping(value = "/api/v1/movies/{movieId}")
     MovieDTO getMovieById(@PathVariable("movieId") Long movieId);
 
-    @GetMapping("/api/v1/movies/{movieId}/exists")
-    boolean existsById(@PathVariable long movieId);
+    @GetMapping("/api/v1/movies/{movieId}/name")
+    String getMovieNameById(@PathVariable("movieId") Long movieId);
+
+    @GetMapping(value = "/api/v1/movies/{movieId}/exists")
+    boolean existsById(@PathVariable("movieId") Long movieId);
 }
