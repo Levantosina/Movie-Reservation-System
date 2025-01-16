fdb19263fe99ed903d22c0bfb68857b51a88a9a7
levan <levantosina1992@gmail.com>
2025-01-16 21:42:17 +0200

    add getSeatsByCinema

--- client/src/main/java/com/movie/client/seatClient/SeatClient.java
+++ client/src/main/java/com/movie/client/seatClient/SeatClient.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author DMITRII LEVKIN on 04/10/2024
@@ -16,7 +17,7 @@ import java.math.BigDecimal;
  */
 @FeignClient(name = "seat",configuration = FeignConfig.class)
 public interface SeatClient {
-    @GetMapping(value= "/api/v1/seats/cinema/{cinemaId}/total-seats")
+    @GetMapping(value = "/api/v1/seats/cinema/{cinemaId}/total-seats")
     TotalSeatsDTO getTotalSeatsByCinemaId(@PathVariable("cinemaId") Long cinemaId);
 
     @GetMapping(value = "/api/v1/seats/{id}")
@@ -31,4 +32,9 @@ public interface SeatClient {
     @GetMapping("/api/v1/seats/price/{seatType}")
     BigDecimal getSeatPriceByType(@PathVariable("seatType") String seatType);
 
+    @GetMapping("/api/v1/seats/cinema/{cinemaId}")
+    List<SeatDTO> getSeatsByCinema(@PathVariable("cinemaId") Long cinemaId);
+
 }
+
+
