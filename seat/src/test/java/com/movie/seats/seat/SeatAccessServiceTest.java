502897b2ff98ad40407e022944e464183f8c0794
levan <levantosina1992@gmail.com>
2025-01-22 09:03:14 +0200

    add ifSeatOccupied & updateSeat tests.

--- seat/src/test/java/com/movie/seats/seat/SeatAccessServiceTest.java
+++ seat/src/test/java/com/movie/seats/seat/SeatAccessServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -98,9 +100,71 @@ class SeatAccessServiceTest extends SeatAbstractDaoUnitTest {
 
     @Test
     void ifSeatOccupied() {
+        Seat seat = Seat.builder()
+                .seatNumber(1)
+                .row("A")
+                .type("VIP")
+                .cinemaId(25L)
+                .isOccupied(true)
+                .build();
+        underTest.insertSeat(seat);
+
+        long seatId = underTest.selectAllSeats()
+                .stream()
+                .map(Seat::getSeatId)
+                .findFirst()
+                .orElseThrow();
+        seat.setSeatId(seatId);
+
+        underTest.isSeatOccupied(seatId);
+
+        Optional<Seat> actual = underTest.selectSeatById(seatId);
+        assertThat(actual).isPresent();
+
     }
 
     @Test
     void updateSeat() {
+
+        Seat seat = Seat.builder()
+                .seatNumber(1)
+                .row("A")
+                .type("VIP")
+                .cinemaId(25L)
+                .isOccupied(false)
+                .build();
+        underTest.insertSeat(seat);
+
+        long seatId = underTest.selectAllSeats()
+                .stream()
+                .map(Seat::getSeatId)
+                .findFirst()
+                .orElseThrow();
+        seat.setSeatId(seatId);
+        System.out.println("Before update: " + seat);
+
+        Integer updateSeatNumber = 2;
+        String updateRow = "B";
+        String updateType = "Standard";
+        Long updateCinemaId = 2L;
+        boolean updateOccupied = false;
+
+        seat.setSeatNumber(updateSeatNumber);
+        seat.setRow(updateRow);
+        seat.setCinemaId(updateCinemaId);
+        seat.setOccupied(updateOccupied);
+        seat.setType(updateType);
+
+
+        underTest.updateSeat(seat);
+        System.out.println("AFTER update: " + seat);
+        Optional<Seat> actual = underTest.selectSeatById(seatId);
+        assertThat(actual).isPresent().hasValueSatisfying(s -> {
+            assertThat(s.getSeatNumber()).isEqualTo(updateSeatNumber);
+            assertThat(s.getRow()).isEqualTo(updateRow);
+            assertThat(s.getType()).isEqualTo(updateType);
+            assertThat(s.getCinemaId()).isEqualTo(updateCinemaId);
+            assertThat(s.isOccupied()).isEqualTo(updateOccupied);
+        });
     }
 }
