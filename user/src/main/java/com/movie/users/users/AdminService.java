c2905672bd4b4841cd8ddb42e998bb1d811a0509
levan <levantosina1992@gmail.com>
2025-02-12 15:24:34 +0200

    add getUserById and getUserByUsername

--- user/src/main/java/com/movie/users/users/AdminService.java
+++ user/src/main/java/com/movie/users/users/AdminService.java
@@ -6,6 +6,7 @@ import com.movie.amqp.RabbitMqMessageProducer;
 import com.movie.client.notification.NotificationRequest;
 import com.movie.common.UserDTO;
 import com.movie.exceptions.DuplicateResourceException;
+import com.movie.exceptions.RequestValidationException;
 import com.movie.exceptions.ResourceNotFoundException;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -45,6 +46,24 @@ public class AdminService {
                 .stream()
                 .map(userDTOMapper)
                 .collect(Collectors.toList());
+    }
+
+    public UserDTO getUserById(Long id) {
+        return userDAO.selectUserById(id)
+                .map(userDTOMapper)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException(
+                                "User with id [%s] not found".
+                                        formatted(id)));
+    }
+
+    public UserDTO getUserByUsername(String username) {
+        return userDAO.selectUserByEmail(username)
+                .map(userDTOMapper)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException(
+                                "User with username [%s] not found".
+                                        formatted(username)));
     }
 
     public void registerAdministrator(AdminRegistrationRequest adminRegistrationRequest, User currentAdmin) {
@@ -145,6 +164,51 @@ public class AdminService {
         } else {
             throw new ResourceNotFoundException("Admin not found with id " + id);
         }
+    }
+
+    public void updateUser(Long userId, UserUpdateRequest userUpdateRequest) {
+
+
+
+        User user = userDAO.selectUserById(userId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User with [%s] not found"
+                                .formatted(userId)));
+
+        boolean changes = false;
+
+        if (userUpdateRequest.firstName() != null && !userUpdateRequest.firstName().equals(user.getFirstName())) {
+            user.setFirstName(userUpdateRequest.firstName());
+            changes = true;
+        }
+        if (userUpdateRequest.lastName() != null && !userUpdateRequest.lastName().equals(user.getLastName())) {
+            user.setLastName(userUpdateRequest.lastName());
+            changes = true;
+        }
+        if (userUpdateRequest.email() != null && !userUpdateRequest.email().equals(user.getEmail())) {
+            if (userDAO.existPersonWithEmail(userUpdateRequest.email())) {
+                throw new DuplicateResourceException("email already taken");
+            }
+            user.setEmail(userUpdateRequest.email());
+            changes = true;
+        }
+        if (!changes) {
+            throw new RequestValidationException("No changes detected");
+        }
+        userDAO.updateUser(user);
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+
+                user.getUserId(),
+                user.getEmail(),
+                String.format("Hi %s, User was updated", user.getFirstName())
+        );
+
+        rabbitMqMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
+        );
     }
 
 }
