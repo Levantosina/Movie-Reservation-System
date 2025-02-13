4d3f15ac54aafbd0e511ef762a25ced0a4f8c3ee
levan <levantosina1992@gmail.com>
2025-02-13 14:55:07 +0200

    add DELETE and PUT requestMatchers for ADMIN

--- movie/src/main/java/com/movie/movie/security/MovieSecurityFilterChainConfig.java
+++ movie/src/main/java/com/movie/movie/security/MovieSecurityFilterChainConfig.java
@@ -33,6 +33,8 @@ public class MovieSecurityFilterChainConfig {
                 .authorizeHttpRequests(auth -> auth
                        .requestMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
                        .requestMatchers(HttpMethod.POST, "/api/v1/movies/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/movies/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/movies/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
